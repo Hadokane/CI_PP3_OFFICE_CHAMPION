df8ef90ee6d9454a66ecc7f1a05c8e6f71337ca5
Hadokane <kanedaveo@gmail.com>
2023-02-23 21:42:11 +0000

    Add js function to remove alerts on click

--- officechampion/static/js/script.js
+++ officechampion/static/js/script.js
@@ -3,3 +3,10 @@ document.addEventListener("DOMContentLoaded", function() {
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
 });
+
+// function to remove alerts onclick
+function removeNode() {
+    const myDiv = document.getElementById("alertCard");
+    const parent = myDiv.parentNode;
+    parent.removeChild(myDiv);
+}
