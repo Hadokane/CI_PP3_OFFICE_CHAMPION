772ad75e5558c38fdde80f5c4fdb89a1fedfb017
Hadokane <kanedaveo@gmail.com>
2023-02-24 12:53:25 +0000

    Add onclick to blue alerts & display username

--- officechampion/static/js/script.js
+++ officechampion/static/js/script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     // sidenav initialization
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
